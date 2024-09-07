b902d4105cbbc9c901d673f50081dc82f806b1aa
Siddharth <siddharthsingh2014@gmail.com>
2024-09-08 03:27:11 +0530

    FEAT: Added error handling middleware function

---
+++ server/src/middleware/errorHandler.ts
@@ -0,0 +1,46 @@
+import type { Request, Response, NextFunction } from "express"
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case 400:
+      res.json({
+        title: "Validation Failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case 404:
+      res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case 401:
+      res.json({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case 403:
+      res.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case 500:
+      res.json({
+        title: "Server Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    default:
+      console.log("No Error, All good!");
+      break;
+  }
+};
