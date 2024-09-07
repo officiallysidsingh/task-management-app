b290b304bd07dcab17fbe138535371d3f11cf714
Siddharth <siddharthsingh2014@gmail.com>
2024-09-08 03:29:28 +0530

    FEAT: Added connectDB, cors and error handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,48 @@
 import express from "express";
-import type{ Request, Response } from "express";
+import type{ Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/dbConnection";
+import cors from "cors";
 
+import { errorHandler } from "./middleware/errorHandler"
+
+
+// Load environment variables
 dotenv.config();
 
-const app = express();
+// Connect to MongoDB
+connectDB()
+
+// Define express app
+const app: Express = express();
+
+// Get PORT Variable from env
 const PORT = process.env.PORT || 8000;
 
+// Middlewares
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+app.use(express.json())
+
+// Routes
+// Health Check Route
 app.get('/', (req: Request, res: Response) => {
   res.json({message: "Hello, World!"});
 });
 
+// User Route
+app.use("/api/user")
+
+// CRUD Routes for Tasks
+app.use("/api/tasks")
+
+// Error Handling
+app.use(errorHandler);
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
